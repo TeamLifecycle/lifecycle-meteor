69a4f76d82b6c6354fda6d415d1fb689f88c22cc
Nathan Ashcraft <ashcrang@mail.uc.edu>
2015-08-20 16:37:43 -0400

    Bump package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'lifecycle:lifecycle',
-	version: '0.0.3',
+	version: '0.0.4',
 	summary: 'Meteor package for interfacing with Lifecycle REST api',
 	git: 'https://github.com/TeamLifecycle/lifecycle-meteor.git',
 	documentation: 'README.md'
@@ -8,12 +8,13 @@ Package.describe({
 
 Package.onUse(function(api) {
 	api.versionsFrom('1.1.0.3');
+	api.use('http');
 	api.addFiles('lifecycle.js');
-	api.export('Lifecycle', 'server');
+	api.export('Lifecycle');
 });
 
 Package.onTest(function(api) {
-	api.use('tinytest', 'server');
-	api.use('lifecycle:lifecycle', 'server');
+	api.use('tinytest');
+	api.use('lifecycle:lifecycle');
 	api.addFiles('lifecycle-tests.js');
 });
