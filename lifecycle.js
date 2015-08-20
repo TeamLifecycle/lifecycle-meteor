1ed858b95bb2300545e75bfb3633899df4848c17
Nathan Ashcraft <ashcrang@mail.uc.edu>
2015-08-20 14:27:20 -0400

    Refactor IdentifyUser to send all data fields.

--- lifecycle.js
+++ lifecycle.js
@@ -19,10 +19,13 @@ Lifecycle.TrackEvent = function (eventId, uniqueId, properties) {
 	});
 };
 
-Lifecycle.IdentifyUser = function (uniqueId, email, browserToken) {
+Lifecycle.IdentifyUser = function (uniqueId, data, extraAttributes) {
 	var key = process.env.LIFECYCLE_API_KEY;
 	if (!key) {
 		return console.log("no LIFECYCLE_API_KEY set");
+	}
+	if (!uniqueId) {
+		return console.log("Must provide `uniqueId` to IdentifyUser");
 	}
 	return HTTP.call("POST", url + "/identify", {
 		headers: {
@@ -30,9 +33,20 @@ Lifecycle.IdentifyUser = function (uniqueId, email, browserToken) {
 			"lifecycle-api-key": key
 		},
 		data: {
-			unique_id: uniqueId,
-			email_address: email,
-			browser_token: browserToken
+			unique_id		: uniqueId,
+			first_name		: data.firstName,
+			last_name		: data.lastName,
+			email_address	: data.email,
+			phone_number	: data.phone,
+			apns_token		: data.apns,
+			gcm_token		: data.gcm,
+			browser_token	: data.browser,
+			street_address	: data.street,
+			city			: data.city,
+			state			: data.state,
+			zip_code		: data.zip,
+			device_type		: data.deviceType
+			attributes 		: extraAttributes || {}
 		}
 	});
 };
