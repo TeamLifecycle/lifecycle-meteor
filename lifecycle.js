84504e60ab879c7917327844e05f7247340b1350
Nathan Ashcraft <ashcrang@mail.uc.edu>
2015-08-20 13:00:49 -0400

    Lifecycle REST api code.

--- lifecycle.js
+++ lifecycle.js
@@ -1,1 +1,38 @@
-// Write your package code here!
+Lifecycle = {};
+var url = process.env.API_URL || 'https://api.lifecycle.io/v1'
+
+Lifecycle.TrackEvent = function (eventId, uniqueId, properties) {
+	var key = process.env.LIFECYCLE_API_KEY;
+	if (!key) {
+		return console.log("no LIFECYCLE_API_KEY set");
+	}
+	return HTTP.call("POST", url + "/track", {
+		headers: {
+			"Content-Type": "application/json",
+			"lifecycle-api-key": key
+		},
+		data: {
+			event_id: eventId,
+			unique_id: uniqueId,
+			properties: properties
+		}
+	});
+};
+
+Lifecycle.IdentifyUser = function (uniqueId, email, browserToken) {
+	var key = process.env.LIFECYCLE_API_KEY;
+	if (!key) {
+		return console.log("no LIFECYCLE_API_KEY set");
+	}
+	return HTTP.call("POST", url + "/identify", {
+		headers: {
+			"Content-Type": "application/json",
+			"lifecycle-api-key": key
+		},
+		data: {
+			unique_id: uniqueId,
+			email_address: email,
+			browser_token: browserToken
+		}
+	});
+};
--- package.js
+++ package.js
@@ -1,22 +1,19 @@
 Package.describe({
-  name: 'lifecycle:lifecycle',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+	name: 'lifecycle:lifecycle',
+	version: '0.0.1',
+	summary: 'Meteor package for interfacing with Lifecycle REST api',
+	git: 'https://github.com/TeamLifecycle/lifecycle-meteor.git',
+	documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.3');
-  api.addFiles('lifecycle.js');
+	api.versionsFrom('1.1.0.3');
+	api.addFiles('lifecycle.js');
+	api.export('Lifecycle', 'server');
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('lifecycle:lifecycle');
-  api.addFiles('lifecycle-tests.js');
+	api.use('tinytest', 'server');
+	api.use('lifecycle:lifecycle', 'server');
+	api.addFiles('lifecycle-tests.js');
 });
