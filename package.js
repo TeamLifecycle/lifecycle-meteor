60556eed1a839a7a173e62e94123013c091d4a1f
Nathan Ashcraft <ashcrang@mail.uc.edu>
2015-08-20 16:57:25 -0400

    Only serve files on server.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'lifecycle:lifecycle',
-	version: '0.0.4',
+	version: '0.0.5',
 	summary: 'Meteor package for interfacing with Lifecycle REST api',
 	git: 'https://github.com/TeamLifecycle/lifecycle-meteor.git',
 	documentation: 'README.md'
@@ -8,13 +8,13 @@ Package.describe({
 
 Package.onUse(function(api) {
 	api.versionsFrom('1.1.0.3');
-	api.use('http');
-	api.addFiles('lifecycle.js');
-	api.export('Lifecycle');
+	api.use('http', 'server');
+	api.addFiles('lifecycle.js', 'server');
+	api.export('Lifecycle', 'server');
 });
 
 Package.onTest(function(api) {
-	api.use('tinytest');
-	api.use('lifecycle:lifecycle');
-	api.addFiles('lifecycle-tests.js');
+	api.use('tinytest', 'server');
+	api.use('lifecycle:lifecycle', 'server');
+	api.addFiles('lifecycle-tests.js', 'server');
 });
